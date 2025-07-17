02a5c67a2e8c461414c8773f5b61a20c86579cda
Dominic Lavezzoli <dom@Dominics-MacBook-Pro.local>
2025-07-17 09:15:29 -0700

    rego

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,7 +12,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "terraformls", "pyright", "gopls" }
+        ensure_installed = { "lua_ls", "terraformls", "pyright", "gopls", "regols" }
       })
     end
   },
